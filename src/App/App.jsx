2d6f5f3dbaea20044c95d6269abfc6e10cf1b374
Alfredo <21030761@itcelaya.edu.mx>
2023-06-30 11:38:10 -0600

    Routes added to handle different pages

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useRoutes, BrowserRouter } from 'react-router-dom'
+
 import { Home_page } from '../components/Home_page';
 import { MyAccount } from '../components/MyAccount';
 import { MyOrder } from '../components/MyOrder';
@@ -6,11 +8,24 @@ import { MyOrders } from '../components/MyOrders';
 import { NotFound } from '../components/NotFound';
 import { SignIn } from '../components/SignIn';
 
+const AppRoutes = () =>{
+  let routes = useRoutes([
+    {path: '/', element: <Home_page/>},
+    {path: '/my-account', element: <MyAccount/>},
+    {path: '/my-order', element: <MyOrder/>},
+    {path: '/my-orders', element: <MyOrders/>},
+    {path: '/sign-in', element: <SignIn/>},
+    {path: '/*', element: <NotFound/>},
+  ]);
+
+  return routes;
+}
+
 function App() {
     return ( 
-      <div>
-        <MyOrder/>
-      </div>      
+      <BrowserRouter>
+        <AppRoutes/>
+      </BrowserRouter>      
     );
 }
 
--- src/components/Home_page/index.jsx
+++ src/components/Home_page/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Home_page() {
     return ( 
       <div>
-
+        Home 
       </div>      
     );
 }
